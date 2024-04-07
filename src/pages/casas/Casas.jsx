eb929aa78c9ba0b950819cf01c58ddc06cd77a1e
freelivecode <mariogilruiz66@gmail.com>
2024-04-07 21:16:15 +0200

    4444

--- src/pages/casas/Casas.jsx
+++ src/pages/casas/Casas.jsx
@@ -1,19 +1,16 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+export const housesGlobal = React.createContext()
 
 
 export default function Casas() {
   const [houses, setHouses] = useState()
-
   const [newHouses, setNewHouses] = useState([]);
   const getHouses = () => {
     axios.get('http://localhost:3000/houses')
       .then(data => setHouses(data.data))
-
   }
-
-  console.log("casas", houses);
 
   useEffect(() => {
 
@@ -21,32 +18,51 @@ export default function Casas() {
 
   }, [])
 
+  // const filtrado = (evento) => {
+  //     const nombre = evento.target.value
+  //     getHouses(nombre)
+  //   }
   const filtrado = (evento) => {
-    // nombre = `${evento.target.value}`
-    getHouses(evento.target.value)
+    const valorInput = evento.target.value.toLowerCase();
+    filtradoCasas(valorInput);
+  }
+
+  const filtradoCasas = (valor) => {
+    const filteredHouses = houses.filter(character =>
+      character.name.toLowerCase().includes(valor)
+    );
+    setNewHouses(filteredHouses);
   }
 
   return (
 
     <>
-      <input type='text' onChange={(e) => filtrado(e)} className='form-data'></input>
+      <input type='text' onChange={(e) => filtrado(e)} className='form-data' />
 
       <div>{houses &&
         <div>
-          {houses.map((house, index) => <Link key={index} to={house.id}>
-            <div>
+          {newHouses.length === 0 ?
+            houses.map((house, index) => <Link key={index} to={house.id}>
               <div>
-                <img src={house.image} alt={house.name} />
-              </div>
-              <h2>{house.name}</h2>
-              <p></p>
+                <div>
+                  <img src={house.image} alt={house.name} />
+                </div>
+                <h2>{house.name}</h2>
+                <p></p>
 
-            </div></Link>
-          )}
+              </div></Link>
+            ) :
+            newHouses.map((house, index) => <Link key={index} to={house.id}>
+              <div>
+                <div>
+                  <img src={house.image} alt={house.name} />
+                </div>
+                <h2>{house.name}</h2>
+                <p></p>
 
+              </div></Link>
+            )}
         </div>}
-      </div>
-    </>
+      </div></>
   )
-
 }
--- src/pages/cronologia/Cronologia.jsx
+++ src/pages/cronologia/Cronologia.jsx
@@ -4,72 +4,92 @@ import './cronologia.css'
 
 
 export default function Cronologia() {
-  
+
   const [characters, setCharacters] = useState([])
-  
+
 
   const getCharacters = () => {
-        axios.get ('http://localhost:3000/characters/')
-        // .then(res => res.json())
-            .then(data => setCharacters(data.data))
+    axios.get('http://localhost:3000/characters/')
+      // .then(res => res.json())
+      .then(data => setCharacters(data.data))
   }
-  
-  useEffect(() => { 
+
+  useEffect(() => {
     getCharacters()
   }, [])
 
-    const [edad,setEdad] = useState(0)
+  const [edad, setEdad] = useState(0)
 
 
   function ordenarItems(items, ascendente) {
+    console.log("hola", ascendente);
     characters.sort(function (a, b) {
       if (ascendente) {
         setEdad(parseInt(b.age))
         return a.age - b.age;
       } else {
         setEdad(parseInt(a.age))
-        
+
         return b.age - a.age;
       }
     });
-    // Devuelve los elementos ordenados
+
     return characters
   }
-  
-  
-// Define el estado inicial del orden y del array de items
-const [ascendente, setAscendente] = useState(true);
-// const [itemsOrdenados, setItemsOrdenados] = useState(characters);
-  
+
+
+  const [ascendente, setAscendente] = useState(true);
+
   const handleClick = () => {
-  // Cambia el estado de orden dependiendo del estado actual
-  setAscendente(!ascendente);
-  // Ordena los elementos según el nuevo estado de orden
-  const nuevosItems = ordenarItems([...characters], !ascendente);
-  // Actualiza el estado del array de items ordenados
-  setCharacters(nuevosItems);
-};
+    setAscendente(!ascendente);
+    const nuevosItems = ordenarItems([...characters], !ascendente);
+    setCharacters(nuevosItems);
+  };
 
 
 
   return (
-  <>
+    <>
+      <div className='b-cronologia__container'>
 
-      <button type='text' onClick={handleClick}>{edad ? edad : 0}</button>
-      
-  <div className='characters-age'>
-      {characters.map((character, index) => character.age &&
-      <div className='character-age' key={index}>
-      <h3>{character.age}</h3>
-      <h3>{character.name}</h3>
-      <div className='character-age_img'>
-      <img src={character.image}></img></div>
-      
-    </div>
-    )}
-      </div>
-      
-  </>
-    
+        <button className='b-btn--cronologia' type='text' onClick={handleClick}>{edad ? edad : 0}</button>
+
+        <div className='b-cronologia__content'>
+
+          <div className='b-pares'>
+
+
+            {characters.map((character, index) => character.age && index % 2 === 0 &&
+              <div className='character-age' key={index}>
+                <p className='pm-0'>{character.age}</p>
+                <h2>{character.name}</h2>
+                <div className='character-age_img'>
+                  <img src={character.image}></img>
+                </div>
+              </div>
+            )}
+
+          </div>
+
+          <div className='b-impares'>
+            {characters.map((character, index) => character.age && index % 2 !== 0 &&
+              <div className='character-age' key={index}>
+                <p className='pm-0'>{character.age}</p>
+                <h2>{character.name}</h2>
+                <div className='character-age_img'>
+                  <img src={character.image}></img>
+                </div>
+              </div>
+
+
+            )}
+
+          </div>
+        </div>
+
+      </div >
+
+    </>
+
   )
 }
